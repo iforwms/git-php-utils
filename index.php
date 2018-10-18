e79759b9bb50a0c89ae2240a6da5067216e371b2
Ifor Waldo Williams <ifor@designedbywaldo.com>
2018-10-18 19:53:14 +0100

    Provide sensible defaults to index.php.

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ require 'vendor/autoload.php';
 require './GitLabel.php';
 require './env.php';
 
-$git = (new GitLabel(
+(new GitLabel(
     $gitApiToken = $githubToken,
     $labelUrl = "https://gist.githubusercontent.com/iforwms/fabbbe262c344cbee3cde07360e84f34/raw/labels.json",
-    $repoOwner = 'DominoChinese',
-    $repoName = 'dc-app'
+    $repoOwner = 'REPO_OWNER',
+    $repoName = 'REPO_NAME'
 ))->synchroniseLabels($forceDelete = false);

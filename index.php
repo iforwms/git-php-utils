9a6d54a7821003bf5080089733be605bdae08c2e
Ifor Waldo Williams <ifor@designedbywaldo.com>
2018-10-19 10:01:46 +0100

    Allow updating of multiple repos.

--- index.php
+++ index.php
@@ -4,9 +4,16 @@ require 'vendor/autoload.php';
 require './GitLabel.php';
 require './env.php';
 
-(new GitLabel(
-    $gitApiToken = $githubToken,
-    $labelUrl = "https://gist.githubusercontent.com/iforwms/fabbbe262c344cbee3cde07360e84f34/raw/labels.json",
-    $repoOwner = 'REPO_OWNER',
-    $repoName = 'REPO_NAME'
-))->synchroniseLabels($forceDelete = false);
+$repos = [
+    ['owner' => 'REPO_OWNER', 'name' => 'REPO_NAME'],
+];
+
+foreach ($repos as $repo) {
+    (new GitLabel(
+        $gitApiToken = $githubToken,
+        $labelUrl = "https://gist.githubusercontent.com/iforwms/fabbbe262c344cbee3cde07360e84f34/raw/labels.json",
+        $repoOwner = $repo['owner'],
+        $repoName = $repo['name']
+    ))
+    ->synchroniseLabels($forceDelete = false);
+}
